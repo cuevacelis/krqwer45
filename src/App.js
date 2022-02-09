0a0e706698c0b07170cb10bac5e6537b68c8742d
cuevacelis <cuevacelis@hotmail.com>
2022-02-08 22:18:16 -0500

    finish change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-/// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Sacar la ropa", done: false },
+    { id: 2, name: "Hacer la cama", done: true },
+    { id: 3, name: "Leer un rato", done: false },
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
-      ],
-      newTask: ''
-    }
+  function submit(event) {
+    setTasks([...tasks, { id: tasks.length + 1, name: newTask, done: false }]);
+    setNewTask("");
+    event.preventDefault();
   }
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
-          </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
-          </form>
-        </div>
+
+  function clickList(task) {
+    //const arrayModify = tasks;
+    //arrayModify[task.id - 1].done = !arrayModify[task.id - 1].done;
+    let array = [...tasks];
+    array.splice(task.id - 1, 1, {
+      id: task.id,
+      name: task.name,
+      done: !task.done,
+    });
+    setTasks(array);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {tasks.map((task) => (
+            <li
+              className={task.done ? "done" : ""}
+              onClick={() => clickList(task)}
+              key={task.id}
+            >
+              {task.name}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={submit}>
+          <input
+            className={newTask.length === 0 ? "error" : ""}
+            type="text"
+            id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </form>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default App;
